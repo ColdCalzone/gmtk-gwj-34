4f1c76c275cf1fe2e3a61a32e6ba7d67300a9bcf
Isaac Astell <isaacjulianastell@gmail.com>
2021-06-12 10:10:18 -0700

    Created simple level manager

---
+++ singetons/LevelManager.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+var levels := {
+	0: {
+		"scene": preload("res://scenes/Test.tscn"),
+		"background": preload("res://icon.png"),
+		"music": ""
+	}
+}
+
+
+
+func load_level(level_num: int) -> void:
+	var scene : PackedScene = levels[level_num]["scene"]
+	var background : StreamTexture = levels[level_num]["background"]
+	
+	get_tree().change_scene_to(scene)
+	
+	# Set up parallax background stuff later, after gmtk
