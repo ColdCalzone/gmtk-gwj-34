5e751ed4a0a8f2f58a0080c001541393220e4abe
Isaac Astell <isaacjulianastell@gmail.com>
2021-06-12 16:16:43 -0700

    Added saving functionality

---
+++ singletons/global.gd
@@ -0,0 +1,42 @@
+extends Node
+
+
+var save := {}
+var save_file := ""
+
+signal on_save
+signal on_load
+
+
+
+func set_data(data, value) -> void:
+	save[data] = value
+
+
+
+func get_data(data):
+	return save[data]
+
+
+
+func save_to_file() -> void:
+	var file := File.new()
+	
+	file.open(save_file, File.WRITE)
+	
+	file.store_string(str(save))
+	file.close()
+
+
+
+func load_from_file(path: String) -> void:
+	var file := File.new()
+	
+	file.open(path, File.READ)
+	
+	var content = file.get_as_text()
+	
+	file.close()
+	
+	save = JSON.parse(content).result
+	save_file = path
--- ui/SelectSaveMenu.gd
+++ ui/SelectSaveMenu.gd
@@ -4,21 +4,42 @@ extends Control
 onready var saves_list := $VBoxContainer/ScrollContainer/SavesList
 
 var load_save_packed = preload("res://ui/custom_widgets/LoadSaveButton.tscn")
+var save_count := 0
 
 
 
 func _ready() -> void:
-	# Replace with loop finding every save file in save folder later
-	for i in range(25):
-		new_save("Save #%s" % [i])
+	var files := []
+	var dir = Directory.new()
+	dir.open("user://")
+	
+	if not dir.dir_exists("saves"):
+		dir.make_dir("saves")
+	
+	dir.change_dir("user://saves/")
+	
+	dir.list_dir_begin()
+	
+	while true:
+		var file : String = dir.get_next()
+		
+		if file == "":
+			break
+		elif file[0] != ".":
+			files.append(file.split(".")[0])
+	
+	dir.list_dir_end()
+	
+	
+	
+	for file in files:
+		save_count += 1
+		new_save(file)
 
 
 
 func load_file(file_name: String) -> void:
-	print("Load ", file_name)
-	LevelManager.load_level(0)
-	# Call level manager to load specific level, and also save file
-	# contents to global variable.
+	Global.load_from_file("user://saves/%s.json" % [file_name])
 
 
 
@@ -32,3 +53,17 @@ func new_save(file_name: String) -> void:
 	button.connect("load_file", self, "load_file")
 	
 	saves_list.add_child(button)
+
+
+
+func _on_NewSaveButton_pressed() -> void:
+	var file := File.new()
+	var content := {
+		"level": 0
+	}
+	
+	file.open("user://saves/save%s.json" % [save_count], File.WRITE)
+	
+	file.store_string(to_json(content))
+	
+	new_save("save%s" % [save_count])
