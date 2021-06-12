175923e00dd8d675839f79d96c7d5707a6bb44f2
Isaac Astell <isaacjulianastell@gmail.com>
2021-06-12 16:31:22 -0700

    Level manager now loads level from save

--- singletons/LevelManager.gd
+++ singletons/LevelManager.gd
@@ -8,12 +8,31 @@ var levels := {
 		"music": ""
 	}
 }
+var current_level := 0
+
+
+
+func _ready() -> void:
+	Global.subscribe(self)
+	Global.connect("on_load", self, "on_load")
+
+
+
+func _save_data() -> void:
+	Global.set_data("level", current_level)
+
+
+
+func on_load() -> void:
+	load_level(Global.get_data("level"))
 
 
 
 func load_level(level_num: int) -> void:
 	var scene : PackedScene = levels[level_num]["scene"]
 	var background : StreamTexture = levels[level_num]["background"]
+	
+	current_level = level_num
 	
 	get_tree().change_scene_to(scene)
 	
